214f62f03d069188469694720da96b103900d21b
Jay Daraniya <76425320+Jay-Tezsure@users.noreply.github.com>
2022-08-03 19:44:08 +0530

    Revert "create and import wallet flow"

--- lib/utils/extensions/size_extension.dart
+++ lib/utils/extensions/size_extension.dart
@@ -5,14 +5,11 @@ extension SizeExtension on num {
   double get width => Get.width * this;
   double get height => Get.height * this;
 
-  /// Calculates the sp (Scalable Pixel) depending on the device's screen size
-  double get sp => this * (Get.width / 4) / 100;
-
   SizedBox get vspace => SizedBox(
-        height: height.toDouble(),
+        height: this.toDouble(),
       );
 
   SizedBox get hspace => SizedBox(
-        width: width.toDouble(),
+        width: this.toDouble(),
       );
 }
